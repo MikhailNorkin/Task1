b0449c0c7a16ce3ca5a81f6bdfe6a3bd44ed875a
Mikhail <mvnorkin@gmail.com>
2022-07-25 00:50:53 +0300

    Task 1 complite

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number1 = 0;
+int number2 = 0;
+Console.Write($"Введите первое число: ");
+bool enter1 = int.TryParse(Console.ReadLine(), out number1);
+if (enter1)
+{ // если правильно ввели первое число, тогда
+    Console.Write($"Введите второе число: ");
+    bool enter2 = int.TryParse(Console.ReadLine(), out number2);
+    if (enter2)
+    { //если правильно ввели второе число, тогда:
+        // проверяем, какое из чисел больше
+        if(number1 > number2)
+        {
+            Console.WriteLine($"max = {number1}");
+        }
+        else 
+        {
+            Console.WriteLine($"max = {number2}");
+        }    
+    }
+    else
+    {
+        Console.WriteLine($"Вы неправильно ввели второе число");
+    }    
+}
+else   
+{
+    Console.WriteLine($"Вы неправильно ввели перове число");
+}    
