4eb86a97ac8c8c57ef549c0789b8f32783aa1e16
Mikhail <mvnorkin@gmail.com>
2022-07-25 17:46:50 +0300

    task3 complite

---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿int number = 0;
+int ostatok = 0;
+
+Console.Write($"Введите число: ");
+bool enter = int.TryParse(Console.ReadLine(), out number);
+if (enter)
+{ // если правильно ввели число, тогда проверим, делиться ли оно на 2 без отстака:
+    ostatok = number % 2;
+    if (ostatok == 0)
+    {
+        Console.WriteLine($"Число является четным");
+    }
+    else
+    {
+        Console.WriteLine($"Число является нечетным");
+    }
+}
+else
+{
+    Console.WriteLine($"Вы неправильно ввели число");
+}
