f6ff0630a4d40c37dd0503ee50fc64df8e83516d
Mikhail <mvnorkin@gmail.com>
2022-07-25 18:26:39 +0300

    task4\ complite

---
+++ Task4/Program.cs
@@ -0,0 +1,31 @@
+﻿int number = 0;
+int ostatok = 0;
+int N = 2;
+
+Console.Write($"Введите число: ");
+bool enter = int.TryParse(Console.ReadLine(), out number);
+if (enter)
+{ 
+    if (number > 0) // проверим число больше нуля или нет:
+    {
+        if (ostatok != 0)
+        { //если без остатка не делится, тогда найдем предыдущее число (оно будет четным):
+            number = number - 1;
+        }
+        Console.WriteLine("Четные числа до вашего числа:");
+        while (N <= number)
+        {
+            Console.Write($"{N} ");   
+            N = N + 2;
+        }
+    }    
+    else
+    {
+      Console.Write("Вы ввели число меньше нуля!");  
+    }
+}
+else
+{
+    Console.WriteLine($"Вы неправильно ввели число");
+}
+Console.WriteLine();
